7f2c4e474d7c4ec15fbd4d7538d12122e51f4c48
asanme <u.alexsanme@gmail.com>
2024-05-16 17:19:31 +0200

    added basic ui template

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	mApp := app.New()
+
+	mainWindow := mApp.NewWindow("Markdown Uploader")
+
+	pathLabel := widget.NewLabel("Obsidian Path")
+	pathEntry := widget.NewEntry()
+	pathEntry.SetPlaceHolder("Enter your Obsidian path...")
+
+	githubLabel := widget.NewLabel("GitHub Repository")
+	githubEntry := widget.NewEntry()
+	githubEntry.SetPlaceHolder("Enter your GitHub URL...")
+
+	grid := container.New(
+		layout.NewFormLayout(),
+		pathLabel,
+		pathEntry,
+		githubLabel,
+		githubEntry,
+	)
+
+	startButton := widget.NewButton(
+		"Watch Changes",
+		func() {
+			fmt.Println("Introduced:", githubEntry.Text)
+		},
+	)
+
+	mainWindow.SetContent(
+		container.NewVBox(
+			grid,
+			startButton,
+		),
+	)
+
+	mainWindow.Show()
+	mainWindow.Resize(fyne.NewSize(200, 100))
+	mApp.Run()
 }
